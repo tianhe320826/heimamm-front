429103f145711f4b1f965277aeaa6b1f8a6f71d3
陈琦涵 <908247643@qq.com>
2020-09-24 21:25:09 +0800

    设置了部分样式，修改一些不合理的地方

--- src/module-hmmm/pages/questions-choice.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container">精选题库</div>
-</template>
-
-<script>
-export default {}
-</script>
-
-<style scoped lang='less'></style>
--- src/module-hmmm/pages/questions.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class='container'>题组管理</div>
-</template>
-
-<script>
-export default {}
-</script>
-
-<style scoped lang='less'></style>
