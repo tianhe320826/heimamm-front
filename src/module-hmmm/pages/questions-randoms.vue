a7044930f7354bef0f77cedc1fd8fe0bf8430206
刘一刀 <272102838@qq.com>
2020-09-26 14:34:08 +0800

    啦啦啦-------德玛没啦!

--- src/module-hmmm/pages/questions-randoms.vue
+++ src/module-hmmm/pages/questions-randoms.vue
@@ -5,21 +5,7 @@
         <el-card shadow="never">
           <!-- 顶部栏 -->
           <div>
-            <div class="please-list-input-suffix">
-              <el-row :gutter="24">
-                <el-col :span="20">
-                  关键字：
-                  <el-input class="Keyword" clearable placeholder="请输入内容" prefix-icon="el-icon-search" v-model="queryInfo.keyword" size="small" @keyup.enter.native="getQuestionData"></el-input>
-                </el-col>
-                <el-col :span="4">
-                  <div class="grid-content bg-purple">
-                    <el-button size="mini" type="info" @click="queryInfo.keyword = ''">清除</el-button>
-                    <el-button size="mini" type="primary" @click="getQuestionData">搜索</el-button>
-                  </div>
-                </el-col>
-              </el-row>
-            </div>
-            <!-- <el-row :gutter="24">
+            <el-row :gutter="24">
               <el-col :span="20">
                 关键字：
                 <el-input class="Keyword" clearable placeholder="请输入内容" prefix-icon="el-icon-search" v-model="queryInfo.keyword" size="small" @keyup.enter.native="getQuestionData"></el-input>
@@ -30,14 +16,12 @@
                   <el-button size="mini" type="primary" @click="getQuestionData">搜索</el-button>
                 </div>
               </el-col>
-            </el-row> -->
+            </el-row>
           </div>
           <!-- 顶部栏 -->
-
           <!-- 提示 -->
           <el-alert :title="`总共${counts}条数据!`" type="info" :closable="false" show-icon></el-alert>
           <!-- 提示 -->
-
           <!-- table表格 -->
           <div>
             <el-table class="randomsTable" :data="isTableData" style="width: 100%">
@@ -49,12 +33,11 @@
                   <el-tag type="warning" v-else>简答</el-tag>
                 </template>
               </el-table-column>
-              <el-table-column class="questionSerial" width="200px" prop="questionIDs[0].number" label="题目编号"></el-table-column>
-              <!-- <el-table-column width="200px" label="题目编号">
+              <el-table-column width="200px" label="题目编号">
                 <template slot-scope="scope">
                   <el-link @click="parameters(item.id)" type="primary" class="randomsLink" v-for="(item, index) in scope.row.questionIDs" :key="index">{{ item.number }} </el-link>
                 </template>
-              </el-table-column> -->
+              </el-table-column>
               <el-table-column width="200px" prop="addTime" label="录入时间"></el-table-column>
               <el-table-column prop="totalSeconds" label="答题时间"></el-table-column>
               <el-table-column prop="accuracyRate" label="正确率"></el-table-column>
@@ -80,7 +63,6 @@
             layout=" prev, pager, next,total, sizes, jumper"
           ></el-pagination>
           <!-- 分页栏 -->
-
           <!-- 弹框 -->
           <el-dialog v-if="dialogShow" title="组件详情" :visible.sync="dialogShow" width="65%">
             <!-- 子组件 -->
@@ -104,6 +86,7 @@ import questionsDetails from '../components/questions-details.vue'
 export default {
   data() {
     return {
+      // search: '',
       isTableData: [],
       // 查询参数对象
       queryInfo: {
@@ -120,14 +103,21 @@ export default {
     questionsDetails
   },
   // 挂载结束
-
-  mounted: function() {},
+  mounted: function () {},
 
   created() {
     this.getQuestionData()
     this.wsm()
   },
   methods: {
+    wsm() {
+      setTimeout(() => {
+        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+        console.log('咦,是王淑敏最好看呀!')
+        console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+      }, 1000)
+      // console.log('旺旺旺,淑敏')
+    },
     async getQuestionData() {
       try {
         const { data } = await randoms(this.queryInfo)
@@ -176,7 +166,7 @@ export default {
             message: '删除成功!111'
           })
         })
-        .catch(err => {
+        .catch((err) => {
           console.dir(err)
           this.$message({
             type: 'info',
@@ -188,7 +178,7 @@ export default {
     // 点击删除按钮时处理isTableData数据
     deleteLIstIndex(id) {
       const deleteData = []
-      this.isTableData.filter(item => {
+      this.isTableData.filter((item) => {
         if (item.id !== id) {
           deleteData.push(item)
         }
@@ -205,7 +195,7 @@ export default {
 }
 </script>
 
-<style scoped lang="less">
+<style scoped lang='less'>
 .container {
   .el-alert {
     margin: 20px 0;
